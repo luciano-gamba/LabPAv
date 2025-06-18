b2cec6d634328c9162e227ad5331348afe6cacbd
MancoloElPato <manuel.guerra@estudiantes.utec.edu.uy>
2025-06-18 16:18:29 -0300

    Agrego cambios en COMPRA y PROMOCION

--- COMPRA.cpp
+++ COMPRA.cpp
@@ -1,6 +1,32 @@
 #include "COMPRA.h"
 
-COMPRA::COMPRA() {
+void compra::agregarProducto(codigoProd string, cant int) {
+    iterator* it = this->misProductos->getIterator();
+    producto* p;
+    bool encontrado = false;
+
+    for(it;it->hasCurrent(); it->next()){
+        p = (producto*)it->getCurrent();
+        if(p->getCodigo() == codigoProd){
+            p->sumarCantidad(cant); //hay que crear la funcion
+            encontrado = true;
+            break;
+        }
+    }
+    if(!encontrado){
+        producto* nuevo = new producto(codigoProd,cant);
+        this->misProductos->add(nuevo);
+    }
+    delete it;
+}
+
+DataCompra* Controlador::mostrarDetalleCompra(){
+    date fechaC = com->fechaCompra();
+    float montoC = com->montoCompra();
+    set<DataProducto*> c = com->getItems();
+    
+    DataCompra* detalle = new DataCompra(fechaC,montoC,c);
+    return detalle;
 }
 
 COMPRA::COMPRA(const COMPRA& orig) {
--- PROMOCION.cpp
+++ PROMOCION.cpp
@@ -1,5 +1,21 @@
 #include "PROMOCION.h"
 
+set<string> Controlador::solicitarListaPromociones(){
+    set<string> promocionesActivas;
+    date fechaSistema = obtenerFechaSistema(); //despues se hara esta funcion
+
+    iterator* it= this->pPromociones->getIterator();
+    while(it->hasCurrent()){
+        Promocion* p = (Promocion*)it->getCurrent();
+        if(p->GetFechaVen() > fechaSistema){
+            promocionesActivas.insert(p->GetNombre());
+        }
+        it->next();
+    }
+    delete it;
+    return promocionesActivas;
+}
+
 PROMOCION::PROMOCION() {
 }
 
